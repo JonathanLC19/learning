063c3fd0e0241ae30ae63f8e42383ab0d5cd9927
Jonathan López <jlopezcastellano19@gmail.com>
2022-11-07 19:41:33 +0000

    añado docstring a la clase

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-#Clases en python
+###Clases en python
+
+##Variable estática
 
 class Book():
+  """
+  Clase para trabajar con libros
+  """
+
   is_electronic = False
 
 book1 = Book()
 
 type(book1)
 
-print(book1)
+#print(book1)
 
-print(Book.is_electronic)
+#print(Book.is_electronic)
+
+##docstrings
+print(Book.__doc__)
