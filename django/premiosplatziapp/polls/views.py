3c00ccfbc011977a2c301184bcae74243a26e9d3
Jonathan López <jlopezcastellano19@gmail.com>
2022-11-15 06:58:44 +0000

    añadidas nuevas vistas y primeros templates a la app. además de superuser

--- django/premiosplatziapp/polls/views.py
+++ django/premiosplatziapp/polls/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
