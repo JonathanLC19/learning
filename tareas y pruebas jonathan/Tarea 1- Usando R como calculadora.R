f40bca71d6354cca058a7c8518b74c91c8a6860a
Jonathan López <102967052+JonathanLC19@users.noreply.github.com>
2023-01-17 08:26:03 +0000

    uploaded files (python/r practice)

---
+++ tareas y pruebas jonathan/Tarea 1- Usando R como calculadora.R
@@ -0,0 +1,24 @@
+#Si hubiéramos empezado a contar segundos a partir de las 12 campanadas que marcan el inicio de 2018, 
+#¿a qué hora de qué día de qué año llegaríamos a los 250 millones de segundos?
+#¡Cuidado con los años bisiestos!
+  
+  #Segundos
+250000000
+
+#Minutos
+  250000000/60
+  4166667
+  #Horas
+  4166667%/%60 #69444 horas
+  4166667%%60 #27 minutos
+  
+  69444%/% 24 #2893 días
+  69444%%24   #12 horas
+  
+  2893%/% 365 #7 años (2 bisiestos)
+  (2893%%365)-2 #336 días
+  365-336
+2018+7  
+ ## Resultado:
+   
+   #2 de dic de 2025 a las 12:27h
